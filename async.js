5dcacdaa8ec2a9da1ad357d0c4431c6ec4cb7f35
Pasindu Perakum <it21160066my.sliit.lk>
2023-11-29 19:40:27 +0530

    async, await

--- async.js
+++ async.js
@@ -91,6 +91,7 @@ order(0, production);
 //error handling
 //the finally handler
 
+/*
 let issShopOpen = true;
 
 let order = (time, work) => {
@@ -135,4 +136,76 @@ order(2000, () => console.log(`${stock.Fruits[0]} was selected`))
   })
   .catch(() => {
     console.log("Customer left");
+  })
+  .finally(() => {
+    if (issShopOpen) console.log("Day ended, shop is closed");
+    else console.log("Shop not opened");
   });
+*/
+
+//async/ await
+
+let issShopOpen = false;
+/*
+let toppingChoice = () => {
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve(console.log("Which topping you  prefer?"));
+    }, 3000);
+  });
+};
+
+async function kitchen() {
+  console.log("task done");
+  await toppingChoice();
+  console.log("task done after await");
+}
+
+kitchen();
+
+console.log("other task working");
+*/
+
+function time(ms) {
+  return new Promise((resolve, reject) => {
+    if (issShopOpen) {
+      setTimeout(resolve, ms);
+    } else {
+      reject(console.log("shop is closed"));
+    }
+  });
+}
+
+async function kitchen() {
+  try {
+    await time(2000);
+    console.log(`${stock.Fruits[0]} was selected`);
+
+    await time(0);
+    console.log("Production has started");
+
+    await time(2000);
+    console.log("The fruits has been chopped");
+
+    await time(1000);
+    console.log(`${stock.liquid[0]} and ${stock.liquid[1]} was added.`);
+
+    await time(1000);
+    console.log("Machine was stated.");
+
+    await time(2000);
+    console.log(`Ice cream placed on ${stock.holder[0]}.`);
+
+    await time(3000);
+    console.log(`${stock.toppings[0]} was added as toppings.`);
+
+    await time(3000);
+    console.log("Ice cream was served");
+  } catch (error) {
+    console.log("Customer left");
+  } finally {
+    console.log("Shop is closed");
+  }
+}
+
+kitchen();
